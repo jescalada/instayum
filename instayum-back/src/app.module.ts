e10979182da626080fbd60502b1262f142defba3
Juan Escalada <juanescalada175@gmail.com>
2023-05-27 21:17:14 -0700

    Hide connection secrets in .env

--- instayum-back/src/app.module.ts
+++ instayum-back/src/app.module.ts
@@ -5,12 +5,14 @@ import { RecipesController } from './recipes/recipes.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RecipeSchema } from 'src/schema/recipe.schema';
 import { RecipesService } from './recipes/recipes.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://juan:Rocco123@cluster0.nxfhi.mongodb.net/?retryWrites=true&w=majority',
-      { dbName: 'instayum' },
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}`,
+      { dbName: process.env.MONGODB_DBNAME },
     ),
     MongooseModule.forFeature([{ name: 'Recipe', schema: RecipeSchema }]),
   ],
