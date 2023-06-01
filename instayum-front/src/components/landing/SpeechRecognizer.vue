29e7006fafe4efa32a21f7387f3e4207048426bc
Juan Escalada <juanescalada175@gmail.com>
2023-05-31 21:57:52 -0700

    Implement preliminary SpeechRecognizer component

---
+++ instayum-front/src/components/landing/SpeechRecognizer.vue
@@ -0,0 +1,202 @@
+<template>
+  <div
+    class="speech-recognizer"
+    :class="{
+      error: error,
+      active: isRecognizing,
+    }"
+    @click="start"
+  ></div>
+
+  <span></span>
+  <div>
+    <h1>Variables</h1>
+    <p>error: {{ error }}</p>
+    <p>isRecognizing: {{ isRecognizing }}</p>
+    <p>runtimeTranscription: {{ runtimeTranscription }}</p>
+    <p>transcription: {{ transcription }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const redirectedEvents = [
+  'audiostart',
+  'audioend',
+  'soundstart',
+  'soundend',
+  'speechstart',
+  'speechend',
+]
+
+const error = ref()
+const isRecognizing = ref<boolean>(false)
+const runtimeTranscription = ref<string>('')
+const transcription = ref<Array>([])
+
+const props = withDefaults(
+  defineProps<{
+    lang: string
+    continuous: boolean
+    interimResults: boolean
+    maxAlternatives: number
+  }>(),
+  {
+    lang: 'en',
+    continuous: true,
+    interimResults: true,
+    maxAlternatives: 1,
+  }
+)
+
+watch(props, (newValue, oldValue) => {
+  for (var key in props) {
+    if (props.hasOwnProperty(key) && typeof props[key] !== 'undefined') {
+      recognition[key] = props[key]
+    }
+  }
+})
+
+const emit = defineEmits([
+  'start',
+  'result',
+  'error',
+  'transcription',
+  'end',
+  'audiostart',
+  'audioend',
+  'soundstart',
+  'soundend',
+  'speechstart',
+  'speechend',
+])
+
+const SpeechRecognition =
+  window['SpeechRecognition'] || window['webkitSpeechRecognition']
+if (!SpeechRecognition) {
+  throw new Error('Speech Recognition is not supported in current browser.')
+}
+const recognition = new SpeechRecognition()
+
+const start = () => {
+  if (isRecognizing.value) {
+    recognition.abort()
+    return
+  }
+  recognition.start()
+}
+
+recognition.lang = props.lang
+recognition.continuous = props.continuous
+recognition.maxAlternatives = props.maxAlternatives
+recognition.interimResults = props.interimResults
+
+recognition.addEventListener('start', () => {
+  error.value = null
+  isRecognizing.value = true
+  console.log('Starting...')
+  emit('start')
+})
+
+recognition.addEventListener('error', (err: string) => {
+  error.value = err
+  isRecognizing.value = false
+  console.log('Error: ', err)
+  emit('error', error)
+})
+
+// Store the runtime captured transciption text
+recognition.addEventListener('result', (event) => {
+  const results = Array.from(event.results)
+  const text = results
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join('')
+  const isFinal = results.some((result) => result.isFinal)
+  if (text) {
+    runtimeTranscription.value = text
+  }
+  if (isFinal) {
+    console.log('Speech ended, sending request to DB.')
+    const finalResult = recognition.stop()
+    console.log('finalResult: ', finalResult)
+  }
+  console.log(event, isFinal)
+})
+
+recognition.addEventListener('nomatch', () => {
+  console.error('Speech not recognized')
+})
+
+recognition.addEventListener('speechend', () => {
+  console.log('Speech has stopped being detected')
+})
+
+recognition.addEventListener('soundend', (event) => {
+  console.log('Sound has stopped being received')
+})
+// On recognition end if a good transciption has been captured
+// emit the transcription event with the whole transciptions list
+// and the last captured sentence than reset the runtime transciption
+recognition.addEventListener('end', () => {
+  isRecognizing.value = false
+
+  if (runtimeTranscription.value !== '') {
+    transcription.value.push(runtimeTranscription.value)
+
+    emit('transcription', {
+      transcription: transcription.value,
+      lastSentence: runtimeTranscription.value,
+    })
+  }
+
+  runtimeTranscription.value = ''
+  emit('end')
+})
+
+// Redirect standard events as Vue component events
+redirectedEvents.forEach((eName): void => {
+  recognition.addEventListener(eName, () => {
+    emit(eName)
+  })
+})
+</script>
+
+<style>
+.speech-recognizer {
+  cursor: pointer;
+  position: relative;
+  background-color: #4db6ac;
+  border-radius: 50%;
+  width: 64px;
+  height: 64px;
+  display: block;
+  transition: all ease-in 250ms;
+}
+.speech-recognizer:hover {
+  background-color: #26a69a;
+}
+
+.speech-recognizer .error {
+  background-color: #bdbdbd;
+}
+
+.speech-recognizer .active {
+  background-color: #ef5350;
+  -webkit-animation: pulse 1.25s infinite cubic-bezier(0.66, 0, 0, 1);
+  -moz-animation: pulse 1.25s infinite cubic-bezier(0.66, 0, 0, 1);
+  animation: pulse 1.25s infinite cubic-bezier(0.66, 0, 0, 1);
+}
+@keyframes pulse {
+  from {
+    box-shadow: 0 0 0 0 rgba(232, 76, 61, 0.7);
+  }
+
+  to {
+    box-shadow: 0 0 0 10px rgba(239, 83, 80, 0.1);
+    background-color: #e53935;
+    transform: scale(0.9);
+  }
+}
+</style>
--- instayum-front/src/pages/LandingPage.vue
+++ instayum-front/src/pages/LandingPage.vue
@@ -1,7 +1,14 @@
 <script lang="ts" setup>
+import SpeechRecognizer from '@/components/landing/SpeechRecognizer.vue'
 import LandingHero from '../components/landing/LandingHero.vue'
 </script>
 
 <template>
   <LandingHero />
+  <SpeechRecognizer
+    :lang="'en'"
+    :continuous="true"
+    :interim-results="true"
+    :max-alternatives="1"
+  />
 </template>
