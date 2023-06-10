8d7a23f6ad647ab535ad159bb39c2c5b5ff9ad90
Juan Escalada <juanescalada175@gmail.com>
2023-06-09 21:52:19 -0700

    Add LandingPage

--- instayum-front/src/pages/LandingPage.vue
+++ instayum-front/src/pages/LandingPage.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import SpeechRecognizer from '@/components/landing/SpeechRecognizer.vue'
 import LandingHero from '../components/landing/LandingHero.vue'
 import MainHeader from '@/components/shared/MainHeader.vue'
 </script>
