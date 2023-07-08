e92666eaaaaffc8aefc936bef983f2c747c3c22d
Juan Escalada <juanescalada175@gmail.com>
2023-07-08 09:57:08 -0700

    Update assets

--- instayum-front/src/components/landing/LandingHero.vue
+++ instayum-front/src/components/landing/LandingHero.vue
@@ -9,13 +9,14 @@ import SearchBox from '../shared/SearchBox.vue'
       <div class="text-indigo-700 max-w-xl">
         <h2 class="mt-6 text-2xl">How to use:</h2>
         <p class="mt-4 mb-4 text-left">
-          <strong>1) Say something</strong>, press record or type in the box<br />
+          <strong>1) Press record</strong> to start the voice recognition<br />
           <strong>2)</strong> Tell us what <strong>ingredients</strong> you have
           available<br />
           <strong>3) Get AI recommendations </strong> on what to cook next!
+          Example: I have some beef, some tomato and some lettuce
         </p>
         <p class="text-left mb-16">
-          <strong>Note: Google Chrome is required</strong> to make use of the
+          <strong>Note: Google Chrome/Safari is required</strong> to make use of the
           voice recognition feature. We're working hard to have this working on
           all platforms!<br /><br /><strong
             >Please enable your microphone</strong
